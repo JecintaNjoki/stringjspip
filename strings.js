1c2b46aefca258c188c655ff5b1a7fff4f2dbd30
JecintaNjoki <jecintanjokikimanii@gmail.com>
2023-04-17 20:50:13 +0300

    add

---
+++ strings.js
@@ -0,0 +1,31 @@
+//Set someAdjective to a string of at least 3 characters and 
+//append it to myStr using the += operator.
+
+//counting duplicate characters in the following string
+ let xters="adsjfdsfsfjsdjfhacabcsbajda"
+ // finding a non repeated character in a string.
+
+ //reversing letters and words
+ let word="I evol uoy os !hcum"
+ 
+
+ //checking whether a string is a palindrome
+
+
+//sorting an array of strings by length
+ //We are given an array of strings, we need to 
+ //sort the array in increasing order of string lengths
+let sentence= ["You", "are", "beautiful", "looking"]
+// for(let i = 0;i<=sentence.length;i++){
+//     if(sentence)
+// }
+let newarray= sentence.sort(a,b)
+console.log
+//checking that a string contains a substring
+//cheking whether two strings are anagrams
+//write a program in Javascript to check if two strings are anagrams 
+//of each other or not
+a= "india"
+b= "nidia"
+a= "hackathon"
+b= "achcthoon"
